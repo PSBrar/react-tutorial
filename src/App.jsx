fd2b39dd253e7b9eba909dff31024db35b088406
PSBrar <prabhjot.brarrr@gmail.com>
2025-05-27 11:44:16 +0100

    cards are made, basic like button is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 //function App() {}
@@ -8,24 +6,42 @@ import './App.css'
 // using the props title below to change what's displayed in the card component
 
 const Card = ({ title }) => {
+    const [hasLiked, setHasLiked] = useState(false);
+
     return(
-        <div>
+        /*<div style={{
+            border: '1px solid black',
+            padding: '20px',
+            margin: '10px',
+            backgroundColor: 'lightblue',
+            borderRadius: '10px',
+            minHeight: '100px',
+        }
+        }>*/
+
+        //curly braces make the thing dynamic
+
+
+        <div className="card">
             <h2>{title}</h2>
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? 'Liked' : 'Like'}
+            </button>
         </div>
     )
 }
 
 const App = () => {
-  return (
-      <div>
-      <h2>Functional arrow component</h2>
 
+
+    return (
+      <div className="card-container">
           <Card title = "F&F" actors = {[{name:'Actors'}]}></Card>
           <Card title = "Lion King"></Card>
           <Card title = "Avatar"></Card>
       </div>
 
-  )
+    )
 }
 
 //props are basically arguments that you can pass to a java function
