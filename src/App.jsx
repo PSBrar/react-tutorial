7dd0da5e8b0ff87a469b0d5e630e9a21954e7fa5
PSBrar <prabhjot.brarrr@gmail.com>
2025-05-29 15:31:36 +0100

    working api call, results shown and spinning loader animation (copied from the internet, didn't make myself) added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,62 @@
-import React, {useState} from 'react'
-import Search from "./components/search.jsx";
+import React, {useState, useEffect} from 'react'
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('')
+    const [errorMsg, setErrorMsg] = useState('');
+    const [movieList, setMovieList] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMovies = async() => {
+        setIsLoading(true);
+        setErrorMsg('');
+
+        try{
+            const endpoint= `${API_BASE_URL}/discover/movie?sort_by+popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if(!response.ok){
+                throw new Error("Error fetching movies");
+            }
+
+            const data = await response.json();
+
+            if(data.Response === "False"){
+                setErrorMsg(data.Error || "Error fetching movies");
+                setMovieList([]);
+                return;
+            }
+
+            setMovieList(data.results || []);
+            console.log(data);
+
+        }
+        catch(e){
+            console.error(e, "ERROR");
+            setErrorMsg("Error fetching movies");
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+
+    useEffect(() => {
+        fetchMovies();
+
+    }, []) //empty deps will mean that will only one run once, at loadtime
 
     return (
         <main>
@@ -14,9 +68,33 @@ const App = () => {
                     <img src={"./public/hero.png"} alt="Hero Baner" />
                     <h1>Find <span className="text-gradient"> Movies </span>
                         You'll Enjoy Without The Hassle</h1>
+
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
 
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <section className={"all-movies"}>
+                    <h2 className="mt-[40px]">All Movies</h2>
+
+
+                    {isLoading ? (
+                        <p className={"text-white"}>
+                            <Spinner/>
+                        </p>
+                    ) : errorMsg ? (
+                        <p className={"text-red-500"}>
+                            {errorMsg}
+                        </p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => {
+                                return (
+                                    <p key={movie.id} className={"text-white"}>{movie.title}</p>
+                                )
+                            })}
+                        </ul>
+                    )
+                    }
+                </section>
             </div>
         </main>
     )
